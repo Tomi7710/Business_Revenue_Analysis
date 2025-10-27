b760ac3e04735d13ab3e42631d8eb88d13b6781f
Tomisin Olofinjana <128741298+Tomi7710@users.noreply.github.com>
2025-10-27 14:19:16 -0230

    Update Sales Insights.sql

--- Sales Insights.sql
+++ Sales Insights.sql
@@ -1,5 +1,6 @@
 USE sales;
 
+
 SELECT * FROM sales.customers;
 
 SELECT SUM(sales.transactions.sales_amount) FROM sales.transactions INNER JOIN sales.date ON sales.transactions.order_date=sales.date.date where sales.date.year=2020;
